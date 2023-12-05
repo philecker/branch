8ffeaf6af653b6bfb50025abe8ee3e888abf0db3
Phil Ecker <hello@philecker.com>
2023-12-05 15:55:29 -0600

    Updated ticket.py to have default prefix

--- ticket.py
+++ ticket.py
@@ -1,13 +1,13 @@
 import re
 import subprocess
 
-def jira_ticket(input_str, ticket_code):
+def jira_ticket(input_str, ticket_prefix, ticket_code):
     formatted_text = input_str.lower()
     formatted_text = re.sub(r'^[^a-zA-Z0-9]+|[^a-zA-Z0-9]+$', '', formatted_text)
     formatted_text = re.sub(r'(?<=.)[^a-zA-Z0-9](?=.|$)', '-', formatted_text)
     formatted_text = re.sub(r"-+", '-', formatted_text)
-    ticket_code = ticket_code.upper()
-    ticket_text = f'issue/AC-{ticket_code}-{formatted_text}'
+    ticket_prefix = ticket_prefix.upper()
+    ticket_text = f'issue/{ticket_prefix}-{ticket_code}-{formatted_text}'
 
     return ticket_text
 
@@ -20,9 +20,11 @@ def clipboard_copy(text):
 
 if __name__ == "__main__":
     input_text = input("Enter the text to format: ")
+    ticket_prefix = input("Enter the ticket prefix (default is 'AC', press Enter to use default): ") or 'AC'
     ticket_code = input("Enter the ticket code: ")
 
-    result_text = jira_ticket(input_text, ticket_code)
+
+    result_text = jira_ticket(input_text, ticket_prefix, ticket_code)
     clipboard_copy(result_text)
 
     print(result_text)
